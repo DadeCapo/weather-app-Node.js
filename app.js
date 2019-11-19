524be4387471013e81f0b5a25da0cd42c5f87655
Simon Choren <dadecapo@hotmail.com>
2019-11-19 13:11:35 -0500

    set up parse of json data from weather darksky.net

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout(() => {
-    console.log('2 Second Timer')
-}, 2000)
+const url = 'https://api.darksky.net/forecast/3753aafb87ef39c1b693861a43914bea/37.8267,-122.4233'
 
-setTimeout(() => {
-    console.log('0 Second Timer')
-}, 0)
-
-console.log('Stopping')
+request({ url: url}, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.currently)    
+})
