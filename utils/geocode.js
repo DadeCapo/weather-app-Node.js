18520148c379a17ef8d15d41f206e1759eefde4f
Simon Choren <dadecapo@hotmail.com>
2019-11-20 13:54:58 -0500

    chaning the callbacks geocode and forecast functions

--- utils/geocode.js
+++ utils/geocode.js
@@ -7,11 +7,11 @@ const geocode = (address, callback) => {
         if (error) {
             callback('Unable to connect to location services')
         } else if (response.body.features.length === 0) {
-            callback('unable to find. Try another search', undefined)
+            callback('Unable to find. Try another search', undefined)
         } else {
             callback(undefined, {
-                latitude: response.body.features[0].center[0],
-                longitude: response.body.features[0].center[1],
+                latitude: response.body.features[0].center[1],
+                longitude: response.body.features[0].center[0],
                 location: response.body.features[0].place_name
             })
         }
